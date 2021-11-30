39898e934d59718d79b1ba680564f9fe49b12d55
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 23:16:49 +0000

    Using \r for Redrawing a Line

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+let symbols = "|/-\|/-\| \n";
+let delay = 0;
+let increment = 500;
+
+for (const char of symbols) {
+    setTimeout(() => {
+        process.stdout.write("\r" + char);
+    }, delay);
+    delay += increment;
+};
